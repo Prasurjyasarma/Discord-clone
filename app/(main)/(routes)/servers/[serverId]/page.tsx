6ab74c5e05f0265b51ea79bc4d8ce3c8dc4fca9e
Prasurjya Sarma <82230151+Prasurjyasarma@users.noreply.github.com>
2024-06-08 20:34:33 +0300

    Ui changes

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,7 @@
 const ServerIdPage = () => {
   return (
-    <h1 className="text-2xl text-rose-500 z-50 text-center mt-5">
-      TODO:Messages
+    <h1 className="text-2xl  text-rose-500 z-50 text-center mt-5">
+      TODO: Messages
     </h1>
   );
 };
