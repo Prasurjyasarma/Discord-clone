f14ede2abdf1ed6bf7c12931de9c09588854c263
Prasurjya Sarma <82230151+Prasurjyasarma@users.noreply.github.com>
2024-06-11 20:35:00 +0300

    upload things add

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -7,7 +7,8 @@ import qs from "query-string";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormField, FormItem } from "../ui/form";
 import { Input } from "../ui/input";
-import { Plus, Smile } from "lucide-react";
+import { Plus, SendHorizonal, Smile } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -21,6 +22,8 @@ const formSchema = z.object({
 });
 
 export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
+  const { onOpen } = useModal();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -52,7 +55,7 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
               <FormControl>
                 <div className="relative p-4 pb-6">
                   <button
-                    onClick={() => {}}
+                    onClick={() => onOpen("messageFile", { apiUrl, query })}
                     className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400
                              hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                     type="button"
@@ -68,8 +71,17 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                     }`}
                     {...field}
                   />
-                  <div className="absolute top-7 right-8">
+                  <div className="absolute top-7 right-20">
                     <Smile />
+                  </div>
+                  <div>
+                    <button
+                      type="submit"
+                      disabled={isLoading}
+                      className="absolute top-7 right-8 hover:text-indigo-400"
+                    >
+                      <SendHorizonal />
+                    </button>
                   </div>
                 </div>
               </FormControl>
