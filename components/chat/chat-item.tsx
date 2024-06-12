15d9f083aa1576464451693d196f3b4b720b0151
Prasurjya Sarma <82230151+Prasurjyasarma@users.noreply.github.com>
2024-06-12 23:07:54 +0300

    Delete message added

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -15,6 +15,7 @@ import { cn } from "@/lib/utils";
 import { Form, FormControl, FormField, FormItem } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatItemProps {
   id: string;
@@ -54,7 +55,7 @@ export const ChatItem = ({
   socketQuery,
 }: ChatItemProps) => {
   const [isEditing, setIsEditing] = useState(false);
-  const [isDeleting, setIsDeleting] = useState(false);
+  const { onOpen } = useModal();
 
   useEffect(() => {
     const handleKeyDown = (event: any) => {
@@ -220,7 +221,15 @@ export const ChatItem = ({
             </ActionTooltip>
           )}
           <ActionTooltip label="delete">
-            <Trash className="cursor-pointer ml-auto w-4 h-4 text-rose-500 hover:text-rose-600 dark:hover:text-rose-300 transition" />
+            <Trash
+              onClick={() =>
+                onOpen("deleteMessage", {
+                  apiUrl: `${socketUrl}/${id}`,
+                  query: socketQuery,
+                })
+              }
+              className="cursor-pointer ml-auto w-4 h-4 text-rose-500 hover:text-rose-600 dark:hover:text-rose-300 transition"
+            />
           </ActionTooltip>
         </div>
       )}
